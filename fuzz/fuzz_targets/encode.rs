ad58f14a3ff99de9412d5983e6cf05c508fdd38c
Thom Chiovoloni <thom@shift.click>
2024-05-04 20:18:17 -0700

    modernize fuzzing stuff

--- fuzz/fuzz_targets/encode.rs
+++ fuzz/fuzz_targets/encode.rs
@@ -1,18 +1,17 @@
-#[macro_use]
-extern crate afl;
+#![no_main]
 
-fn main() {
-    fuzz!(|data: &[u8]| {
-        if data.len() != 0 {
-            let cfg = if (data[0] & 1) == 0 {
-                base16::EncodeLower
-            } else {
-                base16::EncodeUpper
-            };
-            let data = &data[1..];
-            let enc = base16::encode_config(data, cfg);
-            let dec = base16::decode(&enc).unwrap();
-            assert_eq!(data, dec.as_slice());
-        }
-    });
-}
+use libfuzzer_sys::fuzz_target;
+
+fuzz_target!(|data: &[u8]| {
+    if !data.is_empty() {
+        let cfg = if (data[0] & 1) == 0 {
+            base16::EncodeLower
+        } else {
+            base16::EncodeUpper
+        };
+        let data = &data[1..];
+        let enc = base16::encode_config(data, cfg);
+        let dec = base16::decode(&enc).unwrap();
+        assert_eq!(data, dec.as_slice());
+    }
+});
