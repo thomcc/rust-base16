e46102d9a370dff4f264ec5d7c2d691437163d80
Thom Chiovoloni <thom@shift.click>
2024-05-04 22:13:45 -0700

    Rewrite benchmarks to use divan

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,12 +1,7 @@
-#![allow(unknown_lints)]
-
-use criterion::{criterion_group, criterion_main, Criterion, BatchSize, Throughput, ParameterizedBenchmark};
-use rand::prelude::*;
-
-
-const SIZES: &[usize] = &[3, 16, 64, 256, 1024];
+use divan::*;
 
 fn rand_enc_input(sz: usize) -> (Vec<u8>, base16::EncConfig) {
+    use rand::prelude::*;
     let mut rng = thread_rng();
     let mut vec = vec![0u8; sz];
     let cfg = if rng.gen::<bool>() {
@@ -18,67 +13,8 @@ fn rand_enc_input(sz: usize) -> (Vec<u8>, base16::EncConfig) {
     (vec, cfg)
 }
 
-fn batch_size_for_input(i: usize) -> BatchSize {
-    if i < 1024 {
-        BatchSize::SmallInput
-    } else {
-        BatchSize::LargeInput
-    }
-}
-
-fn bench_encode(c: &mut Criterion) {
-    c.bench(
-        "encode to fresh string",
-        ParameterizedBenchmark::new(
-            "encode_config",
-            |b, items| {
-                b.iter_batched(
-                    || rand_enc_input(*items),
-                    |(input, enc)| base16::encode_config(&input, enc),
-                    batch_size_for_input(*items),
-                )
-            },
-            SIZES.iter().cloned(),
-        ).throughput(|bytes| Throughput::Bytes(*bytes as u32)),
-    );
-
-    c.bench(
-        "encode to preallocated string",
-        ParameterizedBenchmark::new(
-            "encode_config_buf",
-            |b, items| {
-                b.iter_batched(
-                    || (rand_enc_input(*items), String::with_capacity(2 * *items)),
-                    |((input, enc), mut buf)| {
-                        buf.truncate(0);
-                        base16::encode_config_buf(&input, enc, &mut buf)
-                    },
-                    batch_size_for_input(*items),
-                )
-            },
-            SIZES.iter().cloned(),
-        ).throughput(|bytes| Throughput::Bytes(*bytes as u32)),
-    );
-
-    c.bench(
-        "encode to slice",
-        ParameterizedBenchmark::new(
-            "encode_config_slice",
-            |b, items| {
-                b.iter_batched(
-                    || (rand_enc_input(*items), vec![0u8; 2 * *items]),
-                    |((input, enc), mut dst)| {
-                        base16::encode_config_slice(&input, enc, &mut dst)
-                    },
-                    batch_size_for_input(*items),
-                )
-            },
-            SIZES.iter().cloned(),
-        ).throughput(|bytes| Throughput::Bytes(*bytes as u32)),
-    );
-}
-
 fn rand_hex_string(size: usize) -> String {
+    use rand::prelude::*;
     let mut rng = thread_rng();
     let mut s = String::with_capacity(size);
     let chars: &[u8] = b"0123456789abcdefABCDEF";
@@ -87,59 +23,72 @@ fn rand_hex_string(size: usize) -> String {
     }
     s
 }
+const SIZES: &[usize] = &[3, 16, 64, 256, 1024];
 
-fn bench_decode(c: &mut Criterion) {
-    c.bench(
-        "decode to fresh vec",
-        ParameterizedBenchmark::new(
-            "decode",
-            |b, items| {
-                b.iter_batched(
-                    || rand_hex_string(*items),
-                    |input| base16::decode(&input),
-                    batch_size_for_input(*items),
-                )
-            },
-            SIZES.iter().cloned(),
-        ).throughput(|bytes| Throughput::Bytes(*bytes as u32 * 2)),
-    );
-
-    c.bench(
-        "decode to preallocated vec",
-        ParameterizedBenchmark::new(
-            "decode_buf",
-            |b, items| {
-                b.iter_batched(
-                    || (rand_hex_string(*items), Vec::with_capacity(*items)),
-                    |(input, mut buf)| {
-                        buf.truncate(0);
-                        base16::decode_buf(&input, &mut buf)
-                    },
-                    batch_size_for_input(*items),
-                )
-            },
-            SIZES.iter().cloned(),
-        ).throughput(|bytes| Throughput::Bytes(*bytes as u32 * 2)),
-    );
-
-    c.bench(
-        "decode to slice",
-        ParameterizedBenchmark::new(
-            "decode_slice",
-            |b, items| {
-                b.iter_batched(
-                    || (rand_hex_string(*items), vec![0u8; *items]),
-                    |(input, mut buf)| {
-                        base16::decode_slice(&input, &mut buf)
-                    },
-                    batch_size_for_input(*items),
-                )
-            },
-            SIZES.iter().cloned(),
-        ).throughput(|bytes| Throughput::Bytes(*bytes as u32 * 2)),
-    );
+#[divan::bench(args = SIZES)]
+fn bench_decode(bencher: Bencher, len: usize) {
+    bencher
+        .counter(len * 2)
+        .with_inputs(|| rand_hex_string(len * 2))
+        .bench_values(|inp| base16::decode(&inp));
 }
 
+#[divan::bench(args = SIZES)]
+fn bench_encode(bencher: Bencher, len: usize) {
+    bencher
+        .counter(len)
+        .with_inputs(|| rand_enc_input(len))
+        .bench_values(|(inp, c)| base16::encode_config(&inp, c));
+}
 
-criterion_group!(benches, bench_encode, bench_decode);
-criterion_main!(benches);
+#[divan::bench(args = SIZES)]
+fn bench_decode_buf(bencher: Bencher, len: usize) {
+    bencher
+        .counter(len * 2)
+        .with_inputs(|| (rand_hex_string(len * 2), Vec::<u8>::with_capacity(len)))
+        .bench_values(|(inp, mut buf)| {
+            let r = base16::decode_buf(&inp, &mut buf);
+            divan::black_box(buf.as_mut_ptr());
+            r
+        });
+}
+
+#[divan::bench(args = SIZES)]
+fn bench_encode_buf(bencher: Bencher, len: usize) {
+    bencher
+        .counter(len)
+        .with_inputs(|| (rand_enc_input(len), String::with_capacity(len * 2)))
+        .bench_values(|((inp, c), mut buf)| {
+            let r = base16::encode_config_buf(&inp, c, &mut buf);
+            divan::black_box(buf.as_mut_ptr());
+            r
+        });
+}
+
+#[divan::bench(args = SIZES)]
+fn bench_decode_slice(bencher: Bencher, len: usize) {
+    bencher
+        .counter(len * 2)
+        .with_inputs(|| (rand_hex_string(len * 2), vec![0u8; len]))
+        .bench_values(|(inp, mut buf)| {
+            let r = base16::decode_slice(&inp, &mut buf[..]);
+            divan::black_box(buf.as_mut_ptr());
+            r
+        });
+}
+
+#[divan::bench(args = SIZES)]
+fn bench_encode_slice(bencher: Bencher, len: usize) {
+    bencher
+        .counter(len)
+        .with_inputs(|| (rand_enc_input(len), vec![0u8; len * 2]))
+        .bench_values(|((inp, c), mut buf)| {
+            let r = base16::encode_config_slice(&inp, c, &mut buf);
+            divan::black_box(buf.as_mut_ptr());
+            r
+        });
+}
+
+fn main() {
+    divan::main();
+}
